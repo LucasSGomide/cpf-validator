9af232c61a63621cddbc97b7d918251c24f7d6e3
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-28 20:26:35 -0300

    chore: finish environment configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,27 @@
-export default {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["airbnb-base", "airbnb-typescript/base"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  parserOptions: {
-    project: "./tsconfig.json",
-  },
-  plugins: ["@typescript-eslint"],
-  rules: {},
-};
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['airbnb-base', 'airbnb-typescript/base'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+        semi: 'off',
+        indent: 'off',
+        '@typescript-eslint/indent': ['error', 4],
+        '@typescript-eslint/semi': ['error', 'never'],
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+}
