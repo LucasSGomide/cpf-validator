e38b30cb450ee8f97a9414f184a5c55ffc6629aa
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-29 00:54:47 -0300

    feat: ensure StringValidation throws if type is not string

--- src/validations/FieldTypeValidation.ts
+++
@@ -1,9 +0,0 @@
-import { IValidation } from './protocols/IValidation'
-
-export class FieldTypeValidation implements IValidation {
-    constructor(readonly field: string, private readonly type: string) {}
-
-    execute(_value: any): Error {
-        return null
-    }
-}
---
+++ src/validations/StringValidation.ts
@@ -0,0 +1,14 @@
+import { InvalidFieldError } from './errors/InvalidFieldError'
+import { IValidation } from './protocols/IValidation'
+
+export class StringValidation implements IValidation {
+    constructor(readonly field: string) {}
+
+    execute(value: any): Error {
+        if (typeof value !== 'string') {
+            throw new InvalidFieldError(this.field)
+        }
+
+        return null
+    }
+}
