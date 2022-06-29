6046ffcebc77f16cba530d8110afee850ea0775e
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-28 23:34:03 -0300

    test: ensure all cpfValidation scenarios are covered

--- src/test/cpf.test.ts
+++ src/test/cpf.test.ts
@@ -9,6 +9,14 @@ describe('CPF', () => {
         expect(result).toBeTruthy()
     })
 
+    test('Deve retornar true para um cpf válido que não tem caracteres especiais', () => {
+        const cpf = '06181380647'
+
+        const result = cpfValidation(cpf)
+
+        expect(result).toBeTruthy()
+    })
+
     test('Deve retornar false para um cpf inválido em que o penúltimo digito deveria ser 0', () => {
         const cpf = '061.813.100-48'
 
@@ -25,23 +33,31 @@ describe('CPF', () => {
         expect(result).toBeFalsy()
     })
 
-    test('Deve retornar false se o cpf for null', () => {
-        const cpf = null
+    test('Deve retornar false se o cpf tiver mais de 11 dígitos', () => {
+        const cpf = '011.111.100-488'
 
         const result = cpfValidation(cpf)
 
         expect(result).toBeFalsy()
     })
 
-    test('Deve retornar false se o cpf possuir caracteres inválidos', () => {
-        const cpf = '&&&&&&&&&&&&&&'
+    test('Deve retornar false se o cpf tiver menos de 11 dígitos', () => {
+        const cpf = '011.111.10'
 
         const result = cpfValidation(cpf)
 
         expect(result).toBeFalsy()
     })
 
-    test('Deve retornar false se o cpf for um objeto', () => {
+    test('Deve retornar false se todos os characteres forem iguais', () => {
+        const cpf = '11111111111'
+
+        const result = cpfValidation(cpf)
+
+        expect(result).toBeFalsy()
+    })
+
+    test('Deve retornar false se o parâmetro passado para validação não for uma string', () => {
         const cpf = {}
 
         const result = cpfValidation(cpf)
