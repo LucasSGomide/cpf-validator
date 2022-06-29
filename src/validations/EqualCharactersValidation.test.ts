6a45ec6f11c008fc4f8c399ad56234bc43e41576
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-06-29 00:09:18 -0300

    refactor: moves sut creation to a factory method

--- src/validations/EqualCharactersValidation.test.ts
+++ src/validations/EqualCharactersValidation.test.ts
@@ -1,13 +1,15 @@
 import { EqualCharactersValidation } from './EqualCharactersValidation'
 
+const makeSut = () => new EqualCharactersValidation('ANY_FIELD')
+
 describe('EqualCharactersValidation', () => {
     test('Deve lançar erro todos os caracteres forem iguais', () => {
-        const sut = new EqualCharactersValidation('ANY_FIELD')
+        const sut = makeSut()
 
         expect(() => sut.execute('aaa')).toThrow()
     })
     test('Deve retornar null se existirem caracteres diferentes', () => {
-        const sut = new EqualCharactersValidation('ANY_FIELD')
+        const sut = makeSut()
 
         const result = sut.execute('ab')
 
