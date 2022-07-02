ad922ded4cd4592d3ad8a84157d7e20088212bd0
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-02 15:00:51 -0300

    feat: ensure CreateOrder creates orderItems correctly

--- src/application/order/CreateOrder.ts
+++ src/application/order/CreateOrder.ts
@@ -1,13 +1,27 @@
-import { IBaseRepository } from '@domain/repository/IBaseRepository'
 import { IUseCase } from '@application/protocols/IUseCase'
 import { Order } from '@domain/entities/Order'
+import { OrderItem } from '@domain/entities/OrderItem'
+import { IOrderItemRepository, IOrderRepository } from '@domain/repository'
 
 export class CreateOrder implements IUseCase {
-    constructor(private orderRepository: IBaseRepository<Order>) {}
+    constructor(
+        private orderRepository: IOrderRepository,
+        private orderItemRepository: IOrderItemRepository
+    ) {}
 
     async execute(order: Order): Promise<Order> {
-        const createdOrder = this.orderRepository.create(order)
+        const createdOrder = await this.orderRepository.create(order)
+        const createdOrderItems = await Promise.all(
+            order.orderItems.map(async (orderItem) => {
+                const createdItem = await this.orderItemRepository.create(
+                    new OrderItem(orderItem)
+                )
+                return createdItem
+            })
+        )
 
-        return Promise.resolve(createdOrder)
+        return Promise.resolve(
+            new Order({ ...createdOrder, orderItems: createdOrderItems })
+        )
     }
 }
