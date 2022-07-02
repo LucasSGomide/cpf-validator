91f15b7983848873761fbf2faff6c0fe8189c871
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-02 16:04:42 -0300

    feat: ensure CreateOrder creates a new order if a user is registred

--- src/application/order/CreateOrder.ts
+++ src/application/order/CreateOrder.ts
@@ -1,15 +1,21 @@
 import { IUseCase } from '@application/protocols/IUseCase'
-import { Order } from '@domain/entities/Order'
-import { OrderItem } from '@domain/entities/OrderItem'
-import { IOrderItemRepository, IOrderRepository } from '@domain/repository'
+import { Order, OrderItem } from '@domain/entities'
+import {
+    IOrderItemRepository,
+    IOrderRepository,
+    IUserRepository,
+} from '@domain/repository'
 
 export class CreateOrder implements IUseCase {
     constructor(
         private orderRepository: IOrderRepository,
-        private orderItemRepository: IOrderItemRepository
+        private orderItemRepository: IOrderItemRepository,
+        private userRepository: IUserRepository
     ) {}
 
     async execute(order: Order): Promise<Order> {
+        await this.userRepository.findById({ id: order.userId })
+
         const createdOrder = await this.orderRepository.create(order)
         const createdOrderItems = await Promise.all(
             order.orderItems.map(async (orderItem) => {
