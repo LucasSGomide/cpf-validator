d8b2394aca5f7ce737a674f11b0bec1b96021cab
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-02 19:26:15 -0300

    refactor: move sut creation to a factory method

--- src/application/user/CreateUser.test.ts
+++ src/application/user/CreateUser.test.ts
@@ -1,22 +1,40 @@
 import { makeCpfValidation } from '@application/validations/cpf-validation/config'
 import { User } from '@domain/entities'
 import { InvalidFieldError } from '@domain/errors/InvalidFieldError'
+import { IUserRepository } from '@domain/repository'
+import { ICpfValidation } from '@domain/validations/ICPfValidation'
 import { CreateUser } from './CreateUser'
 import { makeUserRepositoryMock } from './mocks'
 
+type SutTypes = {
+    cpfValidation: ICpfValidation
+    userRepository: IUserRepository
+    userToCreate: User
+    sut: CreateUser
+}
+
+const makeSut = ({ cpf }: { cpf?: string }): SutTypes => {
+    const validCpf = '473.491.640-33'
+    const cpfValidation = makeCpfValidation()
+    const userRepository = makeUserRepositoryMock()
+    const userToCreate = new User({
+        name: 'any_valid_name',
+        lastName: 'any_valid_lastname',
+        cpf: cpf || validCpf,
+    })
+
+    return {
+        sut: new CreateUser(userRepository, cpfValidation),
+        cpfValidation,
+        userRepository,
+        userToCreate,
+    }
+}
+
 describe('CreateUser', () => {
     test('Deve criar um novo usuário se o mesmo possuir CPF válido', async () => {
-        const cpfValidation = makeCpfValidation()
-        const userRepository = makeUserRepositoryMock()
-        const userToCreate = new User({
-            name: 'any_valid_name',
-            lastName: 'any_valid_lastname',
-            cpf: '473.491.640-33',
-        })
-        const sut = new CreateUser(userRepository, cpfValidation)
-
+        const { sut, userToCreate, userRepository } = makeSut({})
         const createdUser = await sut.execute(userToCreate)
-
         expect(userRepository.create).toBeCalledTimes(1)
         expect(userRepository.create).toBeCalledWith(userToCreate)
         expect(createdUser).toEqual(
@@ -31,17 +49,11 @@ describe('CreateUser', () => {
     })
 
     test('Deve lançar erro se o usuário possuir CPF inválido', async () => {
-        const cpfValidation = makeCpfValidation()
-        const userRepository = makeUserRepositoryMock()
-        const userToCreate = new User({
-            name: 'any_valid_name',
-            lastName: 'any_valid_lastname',
-            cpf: '473.491.640-32',
+        const invalidCpf = '473.491.640-32'
+        const { sut, userToCreate, userRepository } = makeSut({
+            cpf: invalidCpf,
         })
-        const sut = new CreateUser(userRepository, cpfValidation)
-
         const promise = sut.execute(userToCreate)
-
         expect(promise).rejects.toEqual(new InvalidFieldError())
         expect(userRepository.create).not.toBeCalled()
     })
