91024fa388fdd0f1b1f3998e495119d54b502d02
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-02 15:01:16 -0300

    test: ensure CreateOrder creates orderItems correctly

--- src/application/order/CreateOrder.test.ts
+++ src/application/order/CreateOrder.test.ts
@@ -1,34 +1,96 @@
-import { IOrderRepository } from '@domain/repository/IOrderRepository'
-import { CreateOrder } from './CreateOrder'
+import { IOrderRepository, IOrderItemRepository } from '@domain/repository'
 import {
-    createdOrderMock,
+    makeCreatedOrderItemMock,
+    makeNewOrderItemMock,
+    makeOrderItemRepositoryMock,
+} from '@application/order-item/mocks'
+import {
+    makeCreatedOrderMock,
+    makeNewOrderMock,
     makeOrderRepositoryMock,
-    orderToCreate,
-} from './mocks'
+} from '@application/order/mocks'
+import { CreateOrder } from './CreateOrder'
 
 type SutTypes = {
     orderRepository: IOrderRepository
+    orderItemRepository: IOrderItemRepository
     sut: CreateOrder
 }
 
 const makeSut = (): SutTypes => {
     const orderRepository = makeOrderRepositoryMock()
+    const orderItemRepository = makeOrderItemRepositoryMock()
 
     return {
         orderRepository,
-        sut: new CreateOrder(orderRepository),
+        orderItemRepository,
+        sut: new CreateOrder(orderRepository, orderItemRepository),
     }
 }
 
 describe('CreateOrder', () => {
     test('Deve criar um novo pedido', async () => {
         const { sut, orderRepository } = makeSut()
-        jest.spyOn(orderRepository, 'create')
+        const newOrderMock = makeNewOrderMock({
+            number: 1,
+            price: 1,
+            userId: 'any_user_id',
+            orderItems: [],
+        })
+        const createdOrderMock = makeCreatedOrderMock(newOrderMock)
 
-        const createdOrder = await sut.execute(orderToCreate)
-
+        const createdOrder = await sut.execute(newOrderMock)
         expect(orderRepository.create).toBeCalledTimes(1)
-        expect(orderRepository.create).toBeCalledWith(orderToCreate)
+        expect(orderRepository.create).toBeCalledWith(newOrderMock)
+        expect(createdOrder).toEqual(createdOrderMock)
+    })
+
+    test('Deve criar os itens do pedido', async () => {
+        const newOrderItemMock = makeNewOrderItemMock({ quantity: 1 })
+        const newOrderMock = makeNewOrderMock({
+            number: 1,
+            price: 1,
+            userId: 'any_user_id',
+            orderItems: [newOrderItemMock],
+        })
+        const createdOrderMock = makeCreatedOrderMock({
+            ...newOrderMock,
+            orderItems: [makeCreatedOrderItemMock(newOrderItemMock)],
+        })
+
+        const { sut, orderItemRepository } = makeSut()
+        const createdOrder = await sut.execute(newOrderMock)
+        expect(orderItemRepository.create).toBeCalledTimes(
+            newOrderMock.orderItems.length
+        )
+        expect(orderItemRepository.create).toBeCalledWith(
+            newOrderMock.orderItems[0]
+        )
+        expect(createdOrder).toEqual(createdOrderMock)
+    })
+
+    test('Deve criar um pedido com mais de um item', async () => {
+        const newOrderItemMock = makeNewOrderItemMock({ quantity: 1 })
+        const createdOrderItemMock = makeCreatedOrderItemMock(newOrderItemMock)
+        const newOrderMock = makeNewOrderMock({
+            number: 1,
+            price: 1,
+            userId: 'any_user_id',
+            orderItems: [newOrderItemMock, newOrderItemMock, newOrderItemMock],
+        })
+        const createdOrderMock = makeCreatedOrderMock({
+            ...newOrderMock,
+            orderItems: [
+                createdOrderItemMock,
+                createdOrderItemMock,
+                createdOrderItemMock,
+            ],
+        })
+
+        const { sut, orderItemRepository } = makeSut()
+        const createdOrder = await sut.execute(newOrderMock)
+
+        expect(orderItemRepository.create).toBeCalledTimes(3)
         expect(createdOrder).toEqual(createdOrderMock)
     })
 })
--- src/application/order/mocks/index.ts
+++ src/application/order/mocks/index.ts
@@ -1,46 +1,40 @@
-import { Order } from '@domain/entities/Order'
-import { Product } from '@domain/entities/Product'
-import { OrderItem } from '@domain/entities/OrderItem'
-import { IBaseRepository } from '@domain/repository/IBaseRepository'
+import { Order, OrderTypes } from '@domain/entities/Order'
+import { IOrderRepository } from '@domain/repository'
 
-export const orderToCreate = new Order({
-    number: 1,
-    price: 100,
-    userId: 'any_user_id',
-    orderItems: [
-        new OrderItem({
-            quantity: 5,
-            product: new Product({
-                name: 'any_product_name',
-                price: 10,
-                description: 'any_description',
-            }),
-        }),
-    ],
-})
+export const makeNewOrderMock = ({
+    number,
+    price,
+    userId,
+    orderItems,
+}: OrderTypes) =>
+    new Order({
+        number,
+        price,
+        userId,
+        orderItems,
+    })
 
-export const createdOrderMock = new Order({
-    ...orderToCreate,
-    id: 'any_order_id',
-    createdAt: new Date('2022-07-01'),
-    updatedAt: new Date('2022-07-01'),
-    deletedAt: null,
-    orderItems: [
-        ...orderToCreate.orderItems,
-        {
-            ...orderToCreate.orderItems[0],
-            id: 'any_order_item_id',
+export const makeCreatedOrderMock = (order: Order) =>
+    new Order({
+        ...order,
+        id: 'any_order_id',
+        createdAt: new Date('2022-07-01'),
+        updatedAt: new Date('2022-07-01'),
+        deletedAt: null,
+    })
+
+export const makeOrderRepositoryMock = (): IOrderRepository => {
+    const createMock = jest.fn((order: Order) =>
+        Promise.resolve({
+            ...order,
+            id: 'any_order_id',
             createdAt: new Date('2022-07-01'),
             updatedAt: new Date('2022-07-01'),
             deletedAt: null,
-        },
-    ],
-})
+        })
+    )
 
-class OrderRepositoryMock implements IBaseRepository<Order> {
-    async create(order: Order): Promise<Order> {
-        return Promise.resolve(createdOrderMock)
+    return {
+        create: createMock,
     }
 }
-
-export const makeOrderRepositoryMock = () => new OrderRepositoryMock()
