95f13d9b32abe57b4caa307999c981966a85f619
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-09 19:25:43 -0300

    test: ensure Order calculates price with discountCoupon

--- src/domain/entities/Order.test.ts
+++ src/domain/entities/Order.test.ts
@@ -1,3 +1,4 @@
+import { DiscountCoupon } from './DicountCoupon'
 import { Order } from './Order'
 import { OrderItem } from './OrderItem'
 import { Product } from './Product'
@@ -9,13 +10,11 @@ describe('Order', () => {
             price: 10,
             description: 'any_description',
         })
-
         const secondProduct = new Product({
             name: 'any_second_product',
             price: 15,
             description: 'any_description',
         })
-
         const firstItem = new OrderItem({
             quantity: 100,
             product: firstProduct,
@@ -24,14 +23,43 @@ describe('Order', () => {
             quantity: 30,
             product: secondProduct,
         })
-
         const order = new Order({
             orderItems: [firstItem, secondItem],
             userId: 'any_user_id',
         })
-
         const price = order.getPrice()
-
         expect(price).toBe(1450)
+    })
+
+    it('Deve calcular corretamente o valor total de um pedido com cupom de desconto', () => {
+        const firstProduct = new Product({
+            name: 'any_first_product',
+            price: 10,
+            description: 'any_description',
+        })
+        const secondProduct = new Product({
+            name: 'any_second_product',
+            price: 15,
+            description: 'any_description',
+        })
+        const firstItem = new OrderItem({
+            quantity: 100,
+            product: firstProduct,
+        })
+        const secondItem = new OrderItem({
+            quantity: 30,
+            product: secondProduct,
+        })
+        const discountCoupon = new DiscountCoupon({
+            name: 'any',
+            discountPercentage: 10,
+        })
+        const order = new Order({
+            orderItems: [firstItem, secondItem],
+            userId: 'any_user_id',
+            discountCoupon,
+        })
+        const price = order.getPrice()
+        expect(price).toBe(1305)
     })
 })
