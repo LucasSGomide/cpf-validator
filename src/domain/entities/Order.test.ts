11aa2130d71d2e1568c66c86513a3502c59ed2f6
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-09 19:15:34 -0300

    test: ensure Order calculates total price correctly

---
+++ src/domain/entities/Order.test.ts
@@ -0,0 +1,37 @@
+import { Order } from './Order'
+import { OrderItem } from './OrderItem'
+import { Product } from './Product'
+
+describe('Order', () => {
+    it('Deve calcular corretamente o valor total de um pedido sem cupom de desconto', () => {
+        const firstProduct = new Product({
+            name: 'any_first_product',
+            price: 10,
+            description: 'any_description',
+        })
+
+        const secondProduct = new Product({
+            name: 'any_second_product',
+            price: 15,
+            description: 'any_description',
+        })
+
+        const firstItem = new OrderItem({
+            quantity: 100,
+            product: firstProduct,
+        })
+        const secondItem = new OrderItem({
+            quantity: 30,
+            product: secondProduct,
+        })
+
+        const order = new Order({
+            orderItems: [firstItem, secondItem],
+            userId: 'any_user_id',
+        })
+
+        const price = order.getPrice()
+
+        expect(price).toBe(1450)
+    })
+})
