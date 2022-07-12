d1917cf4da5493ceb3b890c1857ba14d9a84f4e3
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-12 01:01:28 -0300

    test: ensure DiscountCoupon returns 0 if discount percentage is 0

--- src/domain/entities/tests/DiscountCoupon.test.ts
+++ src/domain/entities/tests/DiscountCoupon.test.ts
@@ -9,4 +9,13 @@ describe('DiscountCoupon', () => {
         const discount = sut.getDiscount(1000)
         expect(discount).toBe(100)
     })
+
+    it('Deve retornar desconto igual a 0 se o percentual de desconto for 0', () => {
+        const sut = new DiscountCoupon({
+            discountPercentage: 0,
+            name: 'any_name',
+        })
+        const discount = sut.getDiscount(1000)
+        expect(discount).toBe(0)
+    })
 })
