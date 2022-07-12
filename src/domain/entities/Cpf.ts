eef1f8668a08aee63d4955c8351ed6f5242b58f8
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-12 00:35:43 -0300

    refactor: modify Cpf structure to become a value object

---
+++ src/domain/entities/Cpf.ts
@@ -0,0 +1,60 @@
+import { InvalidCpfError } from '@domain/errors/InvalidCpfError'
+
+export class Cpf {
+    private value: string
+    readonly firstFactor = 10
+    readonly secondFactor = 11
+
+    constructor({ value }: { value: string }) {
+        if (!this.validate(value)) throw new InvalidCpfError()
+        this.value = value
+    }
+
+    getValue() {
+        return this.value
+    }
+
+    validate(cpf: string): boolean {
+        const value = this.sanitizeCpf(cpf)
+        if (this.isInvalidLength(value)) return false
+        if (this.isAllEqualCharacters(value)) return false
+        const digitsToValidate = this.getDigitsToValidate(value)
+        const firstDigit = this.calculatesValidDigit(value, this.firstFactor)
+        const secondDigit = this.calculatesValidDigit(value, this.secondFactor)
+        const validDigits = `${firstDigit}${secondDigit}`
+        return validDigits === digitsToValidate
+    }
+
+    private sanitizeCpf(value: string): string {
+        return value.replace(/\D+/g, '')
+    }
+
+    private getDigitsToValidate(cpf: string): string {
+        const lastTwoCharacters = -2
+        return cpf.slice(lastTwoCharacters)
+    }
+
+    private isAllEqualCharacters(cpf: string) {
+        const firstCharacter = cpf[0]
+        return [...cpf].every((character) => character === firstCharacter)
+    }
+
+    private isInvalidLength(cpf: string) {
+        const cpfLength = 11
+        return cpf.length !== cpfLength
+    }
+
+    private calculatesValidDigit(cpf: string, factor: number): number {
+        let total = 0
+        let multiplier = factor
+        const characters = Array.from(cpf)
+        characters.forEach((character) => {
+            if (multiplier > 1) {
+                total += parseInt(character, 10) * multiplier
+                multiplier -= 1
+            }
+        })
+        const rest = total % 11
+        return rest < 2 ? 0 : 11 - rest
+    }
+}
