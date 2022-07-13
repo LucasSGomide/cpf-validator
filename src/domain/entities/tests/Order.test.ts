64d2c66f9facbc17b9d0c8ceafdadea3900c10c1
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-12 21:56:49 -0300

    test: ensure Order calculates price correctly if DiscountCoupon is not expired

--- src/domain/entities/tests/Order.test.ts
+++ src/domain/entities/tests/Order.test.ts
@@ -6,10 +6,39 @@ import { Product } from '../Product'
 
 type SutTypes = {
     value?: string
+    requestDate?: Date
 }
 
-const makeSut = ({ value }: SutTypes) =>
-    new Order({ cpf: value || '473.491.640-33' })
+const makeSut = ({ value, requestDate }: SutTypes) =>
+    new Order({ cpf: value || '473.491.640-33', requestDate })
+
+type MakeBaseOrderTypes = {
+    firstItem: OrderItem
+    secondItem: OrderItem
+}
+
+const makeBaseOrder = (): MakeBaseOrderTypes => {
+    const firstProduct = new Product({
+        name: 'any_first_product',
+        price: 10,
+        description: 'any_description',
+    })
+    const secondProduct = new Product({
+        name: 'any_second_product',
+        price: 15,
+        description: 'any_description',
+    })
+    const firstItem = new OrderItem({
+        quantity: 100,
+        product: firstProduct,
+    })
+    const secondItem = new OrderItem({
+        quantity: 30,
+        product: secondProduct,
+    })
+
+    return { firstItem, secondItem }
+}
 
 describe('Order', () => {
     it('Não deve criar um pedido com CPF inválido', () => {
@@ -18,24 +47,7 @@ describe('Order', () => {
     })
 
     it('Deve calcular corretamente o valor total de um pedido sem cupom de desconto', () => {
-        const firstProduct = new Product({
-            name: 'any_first_product',
-            price: 10,
-            description: 'any_description',
-        })
-        const secondProduct = new Product({
-            name: 'any_second_product',
-            price: 15,
-            description: 'any_description',
-        })
-        const firstItem = new OrderItem({
-            quantity: 100,
-            product: firstProduct,
-        })
-        const secondItem = new OrderItem({
-            quantity: 30,
-            product: secondProduct,
-        })
+        const { firstItem, secondItem } = makeBaseOrder()
         const order = makeSut({})
         order.addItem(firstItem)
         order.addItem(secondItem)
@@ -44,24 +56,7 @@ describe('Order', () => {
     })
 
     it('Deve calcular corretamente o valor total de um pedido com cupom de desconto expirado', () => {
-        const firstProduct = new Product({
-            name: 'any_first_product',
-            price: 10,
-            description: 'any_description',
-        })
-        const secondProduct = new Product({
-            name: 'any_second_product',
-            price: 15,
-            description: 'any_description',
-        })
-        const firstItem = new OrderItem({
-            quantity: 100,
-            product: firstProduct,
-        })
-        const secondItem = new OrderItem({
-            quantity: 30,
-            product: secondProduct,
-        })
+        const { firstItem, secondItem } = makeBaseOrder()
         const discountCoupon = new DiscountCoupon({
             name: 'any',
             percentage: 10,
@@ -73,5 +68,20 @@ describe('Order', () => {
         order.addCoupon(discountCoupon)
         const price = order.getPrice()
         expect(price).toBe(1450)
+    })
+
+    it('Deve calcular corretamente o valor total de um pedido com cupom de desconto válido', () => {
+        const { firstItem, secondItem } = makeBaseOrder()
+        const discountCoupon = new DiscountCoupon({
+            name: 'any',
+            percentage: 10,
+            expireDate: new Date('2022-08-01T10:00:00Z'),
+        })
+        const order = makeSut({ requestDate: new Date('2022-08-01T10:00:00Z') })
+        order.addItem(firstItem)
+        order.addItem(secondItem)
+        order.addCoupon(discountCoupon)
+        const price = order.getPrice()
+        expect(price).toBe(1305)
     })
 })
