f4c73ebca04a6363c47a7e00090c23e64001580d
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-12 21:44:08 -0300

    test: ensure DiscountCoupon validates if a coupon is expired

--- src/domain/entities/tests/DiscountCoupon.test.ts
+++ src/domain/entities/tests/DiscountCoupon.test.ts
@@ -3,8 +3,9 @@ import { DiscountCoupon } from '../DiscountCoupon'
 describe('DiscountCoupon', () => {
     it('Deve calcular corretamente o valor do desconto', () => {
         const sut = new DiscountCoupon({
-            discountPercentage: 10,
+            percentage: 10,
             name: 'any_name',
+            expireDate: new Date('2022-07-12T10:00:00Z'),
         })
         const discount = sut.getDiscount(1000)
         expect(discount).toBe(100)
@@ -12,10 +13,20 @@ describe('DiscountCoupon', () => {
 
     it('Deve retornar desconto igual a 0 se o percentual de desconto for 0', () => {
         const sut = new DiscountCoupon({
-            discountPercentage: 0,
+            percentage: 0,
             name: 'any_name',
         })
         const discount = sut.getDiscount(1000)
         expect(discount).toBe(0)
+    })
+
+    it('Deve retornar true se o cupom estiver expirado', () => {
+        const sut = new DiscountCoupon({
+            percentage: 10,
+            name: 'any_name',
+            expireDate: new Date('2022-06-01T10:00:00Z'),
+        })
+        const isExpired = sut.isExpired(new Date('2022-07-01T10:00:00Z'))
+        expect(isExpired).toBeTruthy()
     })
 })
