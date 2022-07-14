27379242468bd4cec0a56f0b90238fb0366d6a88
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-14 20:26:43 -0300

    test: ensure Dimension calculates density correctly

--- src/domain/entities/tests/Dimension.test.ts
+++ src/domain/entities/tests/Dimension.test.ts
@@ -6,4 +6,11 @@ describe('Dimension', () => {
         const volume = dimension.getVolume()
         expect(volume).toBe(0.003)
     })
+
+    it('Deve retornar a densidade correta de um produto', () => {
+        const weight = 3
+        const dimension = new Dimension({ length: 100, height: 30, width: 10 })
+        const density = dimension.getDensity(weight)
+        expect(density).toBe(100)
+    })
 })
