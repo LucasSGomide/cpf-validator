78b1b3feffd9da53da1fd8d1c2577843bb3d6e9a
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-14 21:50:27 -0300

    test: ensure OrderItem calculates freight price correctly

--- src/domain/entities/tests/OrderItem.test.ts
+++ src/domain/entities/tests/OrderItem.test.ts
@@ -1,5 +1,4 @@
-import { OrderItem } from '../OrderItem'
-import { Product } from '../Product'
+import { Dimension, OrderItem, Product } from '@domain/entities'
 
 describe('OrderItem', () => {
     it('Deve calcular corretamente o valor de um produto', () => {
@@ -9,9 +8,20 @@ describe('OrderItem', () => {
             description: 'any_description',
         })
         const orderItem = new OrderItem({ quantity: 100, product })
-
         const price = orderItem.getPrice()
-
         expect(price).toBe(1000)
+    })
+
+    it('Deve calcular corretamente o preço do frete de um item do pedido', () => {
+        const product = new Product({
+            name: 'any_name',
+            description: 'any_description',
+            price: 10,
+            weight: 1,
+            dimension: new Dimension({ length: 20, height: 15, width: 10 }),
+        })
+        const orderItem = new OrderItem({ quantity: 100, product })
+        const freight = orderItem.getFreight()
+        expect(freight).toBe(999)
     })
 })
