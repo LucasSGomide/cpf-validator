46192064107ef9b5b5ce29e5cbc78880655b353b
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-14 21:46:02 -0300

    test: ensure Product calculates freight price correctly

--- src/domain/entities/tests/Product.test.ts
+++ src/domain/entities/tests/Product.test.ts
@@ -1,5 +1,5 @@
 import { InvalidAttributeError } from '@domain/errors/InvalidAttributeError'
-import { Product } from '../Product'
+import { Product, Dimension } from '@domain/entities'
 
 describe('Product', () => {
     it('Deve lançar erro se o peso do produto for negativo', () => {
@@ -12,5 +12,17 @@ describe('Product', () => {
                     weight: -1,
                 })
         ).toThrow(new InvalidAttributeError('weight'))
+    })
+
+    it('Deve calcular corretamente o preço do frete de um produto', () => {
+        const product = new Product({
+            name: 'any_name',
+            description: 'any_description',
+            price: 10,
+            weight: 1,
+            dimension: new Dimension({ length: 20, height: 15, width: 10 }),
+        })
+        const freight = product.getFreight()
+        expect(freight).toBe(9.99)
     })
 })
