713b8e14dbdd6f64b6ce7441cda94ae3be844f38
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-14 21:15:41 -0300

    feat: ensure Product throws InvalidAttributeError if weight is negative

--- src/domain/entities/Product.ts
+++ src/domain/entities/Product.ts
@@ -1,9 +1,11 @@
+import { InvalidAttributeError } from '@domain/errors/InvalidAttributeError'
 import { BaseEntity } from './BaseEntity'
 
 export class Product extends BaseEntity {
     name: string
     price: number
     description: string
+    weight?: number
 
     constructor({
         id,
@@ -13,10 +15,28 @@ export class Product extends BaseEntity {
         name,
         price,
         description,
-    }: Product) {
+        weight,
+    }: ProductTypes) {
         super({ id, createdAt, deletedAt, updatedAt })
         this.name = name
         this.price = price
         this.description = description
+        this.weight = weight
+        this.validate()
     }
+
+    private validate() {
+        if (this.weight < 0) throw new InvalidAttributeError('weight')
+    }
+}
+
+type ProductTypes = {
+    id?: string
+    createdAt?: Date
+    deletedAt?: Date
+    updatedAt?: Date
+    name: string
+    price: number
+    description: string
+    weight?: number
 }
