3d2b1ad909374941105b931531d3e72b4c4a74cf
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-14 21:03:02 -0300

    test: ensure Dimension throws InvalidAttributeError if any attribute is negative

--- src/domain/entities/tests/Dimension.test.ts
+++ src/domain/entities/tests/Dimension.test.ts
@@ -1,4 +1,5 @@
 import { Dimension } from '@domain/entities/Dimension'
+import { InvalidAttributeError } from '@domain/errors/InvalidAttributeError'
 
 describe('Dimension', () => {
     it('Deve retornar o volume correto de um produto', () => {
@@ -13,4 +14,19 @@ describe('Dimension', () => {
         const density = dimension.getDensity(weight)
         expect(density).toBe(100)
     })
+
+    const dimensions = [
+        { length: -100, height: 30, width: 10 },
+        { length: 100, height: -30, width: 10 },
+        { length: 100, height: 30, width: -10 },
+    ]
+
+    it.each(dimensions)(
+        'Deve lançar erro se existirem dimensões negativas',
+        (dimension) => {
+            expect(() => new Dimension(dimension)).toThrow(
+                new InvalidAttributeError('dimension')
+            )
+        }
+    )
 })
