164bbb3db180ef16e595f0282c5ef41240b2078c
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-16 16:39:04 -0300

    refactor: remove unused repository interfaces

--- src/domain/repository/IOrderItemRepository.ts
+++
@@ -1,4 +0,0 @@
-import { OrderItem } from '@domain/entities'
-import { IBaseRepository } from './IBaseRepository'
-
-export interface IOrderItemRepository extends IBaseRepository<OrderItem> {}
--- src/domain/repository/IUserRepository.ts
+++
@@ -1,4 +0,0 @@
-import { User } from '@domain/entities'
-import { IBaseRepository } from './IBaseRepository'
-
-export interface IUserRepository extends IBaseRepository<User> {}
