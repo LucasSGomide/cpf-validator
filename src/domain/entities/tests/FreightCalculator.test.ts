5f1b46dfbb9a36dbb92e74f42d184e06f19b3320
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-16 13:33:51 -0300

    test: ensure FreightCalculator returns minFreight if freight price is below minFreight value

--- src/domain/entities/tests/FreightCalculator.test.ts
+++ src/domain/entities/tests/FreightCalculator.test.ts
@@ -18,4 +18,16 @@ describe('FreightCalculator', () => {
         const freightPrice = FreightCalculator.calculate(product)
         expect(freightPrice).toBe(0)
     })
+
+    it('Deve retornar o valor mínimo de frete caso o preço do frete seja menor que 10', () => {
+        const product = new Product(
+            'any_id',
+            'any_name',
+            'any_description',
+            10,
+            new Dimension(15, 10, 5, 0.7)
+        )
+        const freightPrice = FreightCalculator.calculate(product)
+        expect(freightPrice).toBe(10)
+    })
 })
