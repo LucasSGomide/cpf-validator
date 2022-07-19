a4ab68a9e6518664ab12232a42e34e1b400d0ca5
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-19 20:05:39 -0300

    refactor: remove database query tests from server

--- src/infra/server/index.ts
+++ src/infra/server/index.ts
@@ -1,20 +1,10 @@
 import express from 'express'
-import { database } from '@infra/database/sqlite/config'
 
 const app = express()
 app.use(express.json())
 
-const testJoin = `
-    SELECT "order".id, "order".cpf, "order".code, "order".price, orderItem.quantity, orderItem.productId
-    FROM "order" INNER JOIN orderItem
-    ON "order".id = orderItem.orderId;
-`
-
-app.get('/teste', (req, res) => {
-    res.send({ message: 'teste' }).json()
-    database.all(testJoin, (err, row) => {
-        console.log(row)
-    })
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello World' }).json()
 })
 
 app.listen(3000)
