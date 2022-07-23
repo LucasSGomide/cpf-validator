1df6c8734f888ed34527cbae0d57bb0f57f16152
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2022-07-23 17:20:09 -0300

    refactor: change database creation script and add seed

--- src/infra/database/config.ts
+++ src/infra/database/config.ts
@@ -1,6 +1,7 @@
 import { DataSource } from 'typeorm'
+import { ProductEntity } from './entities/ProductEntity'
 
-export function CreateDataSource(): DataSource {
+function createDataSource(): DataSource {
     return new DataSource({
         type: 'sqlite',
         database: ':memory:',
@@ -9,4 +10,45 @@ export function CreateDataSource(): DataSource {
         synchronize: true,
         logging: false,
     })
+}
+
+async function seedDatabase(connection: DataSource): Promise<void> {
+    const productSeed = [
+        {
+            name: 'Guitarra',
+            description: 'Guitarra fender modelo XYZ',
+            price: 1000,
+            width: 100,
+            height: 30,
+            length: 10,
+            weight: 3,
+        },
+        {
+            name: 'Amplificador',
+            description: 'Amplificador modelo XYZ',
+            price: 5000,
+            width: 50,
+            height: 50,
+            length: 50,
+            weight: 20,
+        },
+        {
+            name: 'Cabo',
+            description: 'Cabo material XYZ',
+            price: 30,
+            width: 10,
+            height: 10,
+            length: 10,
+            weight: 1,
+        },
+    ]
+    const productRepository = connection.getRepository(ProductEntity)
+    await productRepository.insert(productSeed)
+}
+
+export async function createDatabase(): Promise<DataSource> {
+    const dataSource = createDataSource()
+    const connection = await dataSource.initialize()
+    await seedDatabase(connection)
+    return connection
 }
--- src/infra/server/index.ts
+++ src/infra/server/index.ts
@@ -1,10 +1,17 @@
+import 'reflect-metadata'
 import express from 'express'
+import { createDatabase } from '@infra/database/config'
+import { ProductEntity } from '@infra/database/entities/ProductEntity'
 
 const app = express()
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello World' }).json()
+app.get('/createOrder', async (req, res) => {
+    const connection = await createDatabase()
+    const repository = connection.getRepository(ProductEntity)
+    const products = await repository.find()
+    res.send({ data: products }).json()
+    connection.destroy()
 })
 
 app.listen(3000)
